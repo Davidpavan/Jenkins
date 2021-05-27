090ce60d1522b3e21a3fdfe19fcb3a6e13d56a47
Davidpavan <David@zelarsoft.com>
2021-05-27 15:04:14 +0530

    1.First Commit

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -1,7 +1,7 @@
 def call(Map params = [:]) {
     // Start Default Arguments
     def args = [
-            NEXUS_IP               : '172.31.14.124',
+            NEXUS_IP               : 'some',
     ]
     args << params
 
