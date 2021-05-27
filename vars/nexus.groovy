aa4ea62574c1ebeb7d99c9064e517beb37cc9f10
Davidpavan <David@zelarsoft.com>
2021-05-27 16:13:39 +0530

    1.First Commit

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,4 +1,4 @@
-def nexus() {
-    command = "curl -f -v -u admin:admin123 --upload-file frontend.zip http://192.168.0.84:8081/repository/frontend/frontend1.zip"
+def nexus(COMPONENT) {
+    command = "curl -f -v -u admin:admin123 --upload-file ${COMPONENT}.zip http://192.168.0.84:8081/repository/${COMPONENT}/${COMPONENT}.zip"
     def execute_state=sh(returnStdout: true, script: command)
 }
