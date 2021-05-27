d887c78ea2e37bbf712049796071a4f1c39381f8
Davidpavan <David@zelarsoft.com>
2021-05-27 16:56:30 +0530

    1.First Commit

---
+++ vars/Todo.groovy
@@ -0,0 +1,34 @@
+def call() {
+    pipeline{
+        agent{
+            label 'JAVA'
+        }
+        environment {
+            COMPONENT = "${COMPONENT}"
+        }
+        stages{
+            stage('Downloading dependencies'){
+                steps{
+                    sh '''
+                    npm install && npm run build
+                    '''
+                }
+            }
+            stage('Preparing Artifacts'){
+                steps{
+                    sh '''
+                    zip -r frontend.zip *
+                    '''
+                }
+            }
+            stage('Upload Artifacts'){
+                steps{
+                    script{
+                        upload = new nexus()
+                        upload.nexus ("${COMPONENT}")
+                    }
+                }
+            }
+        }
+    }
+}
