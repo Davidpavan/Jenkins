2ac597c518c4dfd720ed29797454797aa8a80e48
Davidpavan <David@zelarsoft.com>
2021-05-27 17:22:36 +0530

    1.First Commit

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -1,10 +1,21 @@
-def call() {
+def call(Map params = [:]) {
+    // Start Default Arguments
+    def args = [
+            NEXUS_IP               : '192.168.0.84',
+    ]
+    args << params
+
+    // End Default + Required Arguments
     pipeline{
         agent{
             label 'JAVA'
         }
         environment {
-            COMPONENT = "${COMPONENT}"
+            COMPONENT       = "${args.COMPONENT}"
+            NEXUS_IP        = "${args.NEXUS_IP}"
+            PROJECT_NAME    = "${args.PROJECT_NAME}"
+            SLAVE_LABEL     = "${args.SLAVE_LABEL}"
+
         }
         stages{
             stage('Downloading dependencies'){
--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,4 +1,4 @@
 def nexus(COMPONENT) {
-    command = "curl -f -v -u admin:admin123 --upload-file ${COMPONENT}.zip http://192.168.0.84:8081/repository/${COMPONENT}/${COMPONENT}.zip"
+    command = "curl -f -v -u admin:admin123 --upload-file ${COMPONENT}.zip http://${NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip"
     def execute_state=sh(returnStdout: true, script: command)
 }
