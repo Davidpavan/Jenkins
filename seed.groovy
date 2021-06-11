47049dc9ae4c02eabfcdb0342fabfe3008a16280
Davidpavan <David@zelarsoft.com>
2021-06-11 11:46:26 +0530

    1.First Commit

--- seed.groovy
+++ seed.groovy
@@ -5,7 +5,14 @@ folder('CI-Pipelines') {
 
 pipelineJob('CI-Pipelines/frontend-ci') {
     configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+            'triggers' {
+                'hudson.triggers.SCMTrigger' {
+                    'spec'('* * * * 1-5')
+                    'ignorePostCommitHooks'(false)
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
                     'hudson.plugins.git.UserRemoteConfig' {
@@ -27,16 +34,24 @@ pipelineJob('CI-Pipelines/frontend-ci') {
 
 pipelineJob('CI-Pipelines/todo-ci') {
     configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+            'triggers' {
+                'hudson.triggers.SCMTrigger' {
+                    'spec'('* * * * 1-5')
+                    'ignorePostCommitHooks'(false)
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
                     'hudson.plugins.git.UserRemoteConfig' {
                         'url'('https://github.com/Davidpavan/todo.git')
+                        'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                     }
                 }
                 'branches' {
                     'hudson.plugins.git.BranchSpec' {
-                        'name'('*/main')
+                        'name'('*/tags/*')
                     }
                 }
             }
@@ -48,16 +63,24 @@ pipelineJob('CI-Pipelines/todo-ci') {
 
 pipelineJob('CI-Pipelines/login-ci') {
     configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+            'triggers' {
+                'hudson.triggers.SCMTrigger' {
+                    'spec'('* * * * 1-5')
+                    'ignorePostCommitHooks'(false)
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
                     'hudson.plugins.git.UserRemoteConfig' {
                         'url'('https://github.com/Davidpavan/login.git')
+                        'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                     }
                 }
                 'branches' {
                     'hudson.plugins.git.BranchSpec' {
-                        'name'('*/main')
+                        'name'('*/tags/*')
                     }
                 }
             }
@@ -69,16 +92,24 @@ pipelineJob('CI-Pipelines/login-ci') {
 
 pipelineJob('CI-Pipelines/user-ci') {
     configure { flowdefinition ->
-        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+            'triggers' {
+                'hudson.triggers.SCMTrigger' {
+                    'spec'('* * * * 1-5')
+                    'ignorePostCommitHooks'(false)
+                }
+            }
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
                     'hudson.plugins.git.UserRemoteConfig' {
                         'url'('https://github.com/Davidpavan/users.git')
+                        'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                     }
                 }
                 'branches' {
                     'hudson.plugins.git.BranchSpec' {
-                        'name'('*/main')
+                        'name'('*/tags/*')
                     }
                 }
             }
