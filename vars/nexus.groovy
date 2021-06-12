e6f152e12d9733875ec826f49d3155d32ad27443
Davidpavan <David@zelarsoft.com>
2021-06-12 10:39:52 +0530

    1.First Commit

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,23 +1,30 @@
 def nexus(COMPONENT) {
-    command = "curl -f -v -u admin:admin123 --upload-file ${COMPONENT}.zip http://${NEXUS_IP}:8081/repository/${COMPONENT}/${COMPONENT}.zip"
+    get branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+
+    command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
 }
 
 def make_artifacts(APP_TYPE, COMPONENT){
+    get branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX"){
-        command = "echo ${COMPONENT} && zip -r ${COMPONENT}.zip *"
+        command = "echo ${COMPONENT} && zip -r ${FILENAME} ."
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "GO"){
-        command = "zip -r ${COMPONENT}.zip *"
+        command = "zip -r ${FILENAME} ."
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "JAVA"){
-        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${COMPONENT}.zip ${COMPONENT}.jar"
+        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "NODEJS"){
-        command = "zip -r ${COMPONENT}.zip node_modules server.js"
+        command = "zip -r ${FILENAME} node_modules server.js"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
@@ -25,8 +32,7 @@ def make_artifacts(APP_TYPE, COMPONENT){
 
 def code_build(APP_TYPE, COMPONENT){
     if(APP_TYPE == "NGINX"){
-        //command = "npm install && npm run build"
-        command = "env"
+        command = "npm install && npm run build"
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "GO"){
