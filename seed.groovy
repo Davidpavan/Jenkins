a7a0f0622826298ba951206504517689681ed967
Davidpavan <David@zelarsoft.com>
2021-06-12 11:49:48 +0530

    1.First Commit

--- seed.groovy
+++ seed.groovy
@@ -13,7 +13,7 @@ for (i in 0..count) {
           flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
               'triggers' {
                   'hudson.triggers.SCMTrigger' {
-                      'scm'('* * * * 1-5')
+                      'spec'('* * * * 1-5')
                       'ignorePostCommitHooks'(false)
                   }
               }
