092af9e00f849f9ea17035d41b457898d53742de
Davidpavan <David@zelarsoft.com>
2021-06-12 14:28:42 +0530

    1.First Commit

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,34 +1,34 @@
-//def nexus(COMPONENT) {
-   // get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-    //def get_branch_exec=sh(returnStdout: true, script: get_branch)
-    //def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+def nexus(COMPONENT) {
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
 
-    //command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
-    //def execute_state=sh(returnStdout: true, script: command)
-//}
+    command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://${NEXUS_IP}:8081/repository/${COMPONENT}/${FILENAME}"
+    def execute_state=sh(returnStdout: true, script: command)
+}
 
-//def make_artifacts(APP_TYPE, COMPONENT){
-  //  get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-    //def get_branch_exec=sh(returnStdout: true, script: get_branch)
-    //def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-    //if(APP_TYPE == "NGINX"){
-      //  command = "echo ${COMPONENT} && zip -r ${FILENAME} ."
-        //def execute_com=sh(returnStdout: true, script: command)
-        //print execute_com
-    //} else if(APP_TYPE == "GO"){
-      //  command = "zip -r ${FILENAME} ."
-        //def execute_com=sh(returnStdout: true, script: command)
-        //print execute_com
-    //} else if(APP_TYPE == "JAVA"){
-      //  command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
-        //def execute_com=sh(returnStdout: true, script: command)
-        //print execute_com
-    //} else if(APP_TYPE == "NODEJS"){
-      //  command = "zip -r ${FILENAME} node_modules server.js"
-        //def execute_com=sh(returnStdout: true, script: command)
-        //print execute_com
-    //}
-//}
+def make_artifacts(APP_TYPE, COMPONENT){
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
+    def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
+    if(APP_TYPE == "NGINX"){
+        command = "echo ${COMPONENT} && zip -r ${FILENAME} ."
+        def execute_com=sh(returnStdout: true, script: command)
+        print execute_com
+    } else if(APP_TYPE == "GO"){
+        command = "zip -r ${FILENAME} ."
+        def execute_com=sh(returnStdout: true, script: command)
+        print execute_com
+    } else if(APP_TYPE == "JAVA"){
+        command = "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
+        def execute_com=sh(returnStdout: true, script: command)
+        print execute_com
+    } else if(APP_TYPE == "NODEJS"){
+        command = "zip -r ${FILENAME} node_modules server.js"
+        def execute_com=sh(returnStdout: true, script: command)
+        print execute_com
+    }
+}
 
 
 def code_build(APP_TYPE, COMPONENT){
