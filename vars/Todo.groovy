f4a6f52bebfd7bab0d65ee7449214be91068816d
Davidpavan <David@zelarsoft.com>
2021-07-05 17:37:51 +0530

    jenkinsfile

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -58,7 +58,9 @@ def call(Map params = [:]) {
 //            }
             stage('Deploy to Env'){
                 steps{
-                    ls
+                    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
+                    env.get_branch_exec=sh(returnStdout: true, script: get_branch)
+                    build job: 'Deployment Pipeline', parameters: [string(name: 'Env', value: 'dev'), string(name: 'COMPONENT', value: "${COMPONENT}"), string(name: 'VERSION', value: '0.0.0')]
                 }
             }
 
